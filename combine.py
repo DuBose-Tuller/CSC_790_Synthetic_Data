c09c01be237080bfeb680bcdbed7a60964c2149e
DuBose Tuller <dubose@DuBoses-MBP.wireless.davidson.edu>
2025-10-03 15:41:54 -0400

    fix dirs

--- combine.py
+++ combine.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
-original_df = pd.read_csv("fitness_dataset.csv")
-synthetic_df = pd.read_csv("fitness_synthetic.csv")
+original_df = pd.read_csv("data/fitness_dataset.csv")
+synthetic_df = pd.read_csv("data/fitness_synthetic.csv")
 
 combined_df = pd.concat([original_df, synthetic_df], axis=0)
-combined_df.to_csv("fitness_combined.csv")
+combined_df.to_csv("data/fitness_combined.csv")
