37678413dc30d7d67c679cc708478da8b3c9f011
DuBose Tuller <dubose@DuBoses-MBP.wireless.davidson.edu>
2025-10-03 13:28:06 -0400

    use the right file for split

--- split.py
+++ split.py
@@ -1,10 +1,10 @@
 import pandas as pd
 
-full_df = pd.read_csv("fitness_combined.csv")
+full_df = pd.read_csv("fitness_dataset.csv")
 target = "is_fit"
 minority_value = 1
 
 minority_rows = full_df[full_df[target] == minority_value]
 print(len(minority_rows) / len(full_df))
 
-# minority_rows.to_csv("fitness_minority_rows.csv")
+minority_rows.to_csv("fitness_minority_rows.csv")
