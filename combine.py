a43cc55e6122c9d85148760398b69e47305cfa48
DuBose Tuller <dubose@DuBoses-MBP.wireless.davidson.edu>
2025-10-05 11:53:55 -0400

    fix cols and clear outs

--- combine.py
+++ combine.py
@@ -2,6 +2,7 @@ import pandas as pd
 
 original_df = pd.read_csv("data/fitness_dataset.csv")
 synthetic_df = pd.read_csv("data/fitness_synthetic.csv")
+synthetic_df = synthetic_df.drop(columns=['Unnamed: 0'])
 
 combined_df = pd.concat([original_df, synthetic_df], axis=0)
 combined_df.to_csv("data/fitness_combined.csv")
